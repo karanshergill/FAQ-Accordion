653d6d07a8c7468ea3f4728662130fc4ec0636a9
Karan <email@karanshergill.com>
2024-07-18 19:37:17 +0530

    fixed mobile view

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -14,16 +14,16 @@ export const Faq = () => {
             <div className='absolute h-1/2 inset-x-0 top-0 bg-cover bg-center bg-background-mobile md:bg-background-desktop'></div>
             <div className='absolute h-1/2 inset-x-0 bottom-0 bg-Light-Pink'></div>
             <div className='relative flex items-center justify-around h-screen font-Work-Sans'>
-                <div className='flex flex-col bg-White rounded-xl w-2/5'>
-                    <div className='flex gap-4 pt-12 px-10'>
+                <div className='flex flex-col bg-White rounded-xl w-5/6 md:w-2/5'>
+                    <div className='flex gap-4 pt-8 px-6 md:pt-12 md:px-10'>
                         <img src="/src/assets/images/icon-star.svg" alt="Icon"></img>
                         <h1 className='text-4xl font-bold text-Dark-Purple'>FAQs</h1>
                     </div>
-                    <div className='flex flex-col py-10 px-10'>
+                    <div className='flex flex-col px-6 md:py-6 md:px-10'>
                         {faqData.map((item, index) => (
-                            <div className='flex flex-col border-b-2 py-6 gap-4 last:border-none' key={index}>
+                            <div className='flex flex-col border-b-2 py-4 md:py-6 last:border-none' key={index}>
                                 <div className='flex justify-between' onClick={() => toggleAccordion(index)}>
-                                    <h3 className='text-lg font-bold text-Dark-Purple'>{item.question}</h3>
+                                    <h3 className='font-bold md:text-lg text-Dark-Purple'>{item.question}</h3>
                                     <img src={activeIndex === index ? '/src/assets/images/icon-minus.svg' : '/src/assets/images/icon-plus.svg'} alt=""/>
                                 </div>
                                 {activeIndex === index && (
@@ -32,11 +32,8 @@ export const Faq = () => {
                             </div>
                         ))}
                     </div>
-
                 </div>
             </div>
         </div>
-
-
     )
 }
